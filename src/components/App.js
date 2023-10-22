a29fc9f1150c70d2ac20e907dfc1c9b87ef817a8
Kipleting Edwin <kipletingedwin4@gmail.com>
2023-10-22 21:35:40 +0300

    style my venue.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import '../App.css';
 import Navbar from './Navbar';
+import Venue from './Venue';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Venue/>
     </div>
   );
 }
---
+++ src/components/Venue.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Venue() {
+  return (
+    <section className='venue-container'>
+        <div className='venue'> 
+        <h1> That's a wrap! </h1>
+        <p> October 5 & 6 </p>
+        <a className='venue-link' href=''>Amsterdam, Beurs van Berlage</a>
+        <button> Videos coming soon</button>
+        </div>
+        <div className='description'> 
+            <p> Rails World was a two-day, two track community conference featuring 
+               technical talks, demos, workshops, networking, and keynotes about the latest 
+                features and best practices in Rails development. There were 700+ Rails 
+                 developers in attendance, 29 speakers, and one big birthday celebration. </p>
+        </div>
+
+    </section>
+   
+  )
+}
+
+export default Venue
