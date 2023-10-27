79629143d423d245128dac6387827d2916c52050
Kipleting Edwin <kipletingedwin4@gmail.com>
2023-10-27 08:25:42 +0300

    add fontawesome icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
   return (
     <footer>
         <div className='myFooter'>
-            <p> This website was created by <a href='https://www.linkedin.com/in/shona-chan/'> Shona Chan</a>, a junior developer from the Rails community.
+            <p> This website was created by <a href='https://www.linkedin.com/in/shona-chan/'> Shona Chan</a>, a junior developer from the Rails community.<br />
                 It was commissioned by the Rails Foundation, designed by <a href='https://katya-sitko.netlify.app/'>Katya Sitko</a> and created under
                  the mentorship of <a href='https://radioactivetoy.tech/'> Ayush Newatia. </a> </p>
                  <ul>
-                    <li> <FontAwesomeIcon icon="fa-brands fa-twitter" /> </li>
-                    <li> <FontAwesomeIcon icon="fa-brands fa-twitter" /></li>
+                    <li> <FontAwesomeIcon icon={faTwitter} /> </li>
+                    <li> <FontAwesomeIcon icon={faLinkedin} /></li>
                  </ul>
                  <ul>
                     <li> 
