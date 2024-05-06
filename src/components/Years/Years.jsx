a826f0e5ef6d0a608a99caa3b0b46660ce7b513b
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-05-06 14:33:31 +0300

    finalize on speakers

--- src/components/Years/Years.jsx
+++ src/components/Years/Years.jsx
@@ -4,7 +4,24 @@ import styles from './Years.module.css';
 function Years() {
   return (
     <section className={styles.container}> 
-        Years
+        {/* <div className={styles.outerContainer}> */}
+            <div className={styles.innerContainer}>
+                <div className={styles.left}>
+                    <h2>20 Years of <br/>Rails </h2>
+                </div>
+
+                <div className={styles.right}>
+                    <p> We celebrated an incredible milestone together - Rails turned 20! With <br/>
+                     an amazing first 20 years under our belt, we now look forward to what <br/>
+                      the next 20 will bring. <br/>
+                    (Yes, there was cake.) </p>
+                    <button>Click here.</button>
+
+
+                </div>
+
+            {/* </div> */}
+        </div>
     </section>
   )
 }
